47f7afa48165fba79ce51b58e2ceeab4b6884035
lazywizar <varun@readypulse.com>
2018-12-16 11:00:13 -0800

    Remove GuidedLDA downloaded

--- GuidedLDA/guidedlda/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, unicode_literals  # noqa
-
-import logging
-
-from guidedlda.guidedlda import GuidedLDA  # noqa
-import guidedlda.datasets  # noqa
-
-logging.getLogger('guidedlda').addHandler(logging.NullHandler())
